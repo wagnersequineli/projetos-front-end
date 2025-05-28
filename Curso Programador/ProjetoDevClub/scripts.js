2368606bd95474f8c783fc1bb3bbac9faac72b6f
Wagner Sequineli <wagnersequineli_@hotmail.com>
2025-05-28 15:22:13 -0300

    Arrumando sintaxe

--- Curso Programador/ProjetoDevClub/scripts.js
+++
@@ -1,9 +0,0 @@
-let imagem = document.querySelector(".imagem-copo")
-let circulo = document.querySelector(".circulo")
-
-function trocaImagem(endereco){    
-    imagem.src = endereco
-}
-function trocarACor(cor){
-    circulo.style.background = cor
-}
--- Nike/scripts.js
+++ Nike/scripts.js
@@ -3,13 +3,13 @@ function mudarVisual(cor) {
     const body = document.body;
 
     if (cor === 'verde') {
-        imagemTenis.src = '/img/nike1.png';
+        imagemTenis.src = '/Nike/img/nike1.png';
         body.style.backgroundColor = '#ccee66'; 
     } else if (cor === 'rosa') {
-        imagemTenis.src = '/img/nike3.png';
+        imagemTenis.src = '/Nike/img/nike3.png';
         body.style.backgroundColor = '#ff9eb5'; 
     } else if (cor === 'azul') {
-        imagemTenis.src = '/img/nike2.png';
+        imagemTenis.src = '/Nike/img/nike2.png';
         body.style.backgroundColor = '#58cccd'; 
     }
 }
